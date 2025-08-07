5ee3db0f7ba55b9f0bea3d440a2d450be3eefa2d
PhanThanhTungg <166277205+PhanThanhTungg@users.noreply.github.com>
2025-08-07 22:12:43 +0700

    feat(users): refactor user authentication and info retrieval logic; improve user handling in UsersService and UsersController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
-import { ClerkAuthGuard } from 'src/common/guards/clerk-auth.guard';
+import { Public } from 'src/common/decorators/public.decorator';
+import { Request } from 'express';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { ClerkAuthGuard } from 'src/common/guards/clerk-auth.guard';
 
-@ApiBearerAuth()
-@UseGuards(ClerkAuthGuard)
 @Controller('users')
 export class UsersController {
 
   constructor(private readonly usersService: UsersService) {}
-
+  
+  @UseGuards(ClerkAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Auth user with clerk // Get user info' })
   @Post('auth')
   async authUser(@CurrentUser() user: any) {
-    return this.usersService.authUser(user);
+    if(!user) {
+      throw new Error('User not authenticated');
+    }
+    return user;
   }
 
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,39 +1,38 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ClerkClient, verifyToken } from '@clerk/backend';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { EnvService } from 'src/shared/env/env.service';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {
+export class UsersService  {
 
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject('ClerkClient')
+    private readonly clerkClient: ClerkClient,
+    private readonly env: EnvService,
+  ) { }
 
-  async authUser(userRequest: any) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userRequest.id,
-      },
-    });
-
-    if (!user) {
-      const newUser = await this.prisma.user.create({
-        data: {
-          clerkId: userRequest.id,
-        },
-      });
-
-      return newUser;
-    }
-
-    return user;
+  authUser(userRequest: Request) {
   }
 
   async getUserInfo(id: string) {
     const user = await this.prisma.user.findUnique({
       where: {
-        id,
+        clerkId: id,
       },
     });
 
-    if (!user) throw new NotFoundException('User not found');
+    if (!user){
+      const newUser = await this.prisma.user.create({
+        data: {
+          clerkId: id,
+        },
+      });
+      return newUser;
+    }
 
     return user;
   }
